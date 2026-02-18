aab75a3a9319ce5192e3b992bfb248ae9c2bf8a5
NASPA365 <ignass221@outlook.com>
2026-02-18 13:07:07 +0000

    vinnsla.ferd klasinn kominn með layout að properties eru heiti ferðar, afangastaður og dagsetning

--- Verkefni3/src/main/java/hi/verkefni/vinnsla/Ferd.java
+++ Verkefni3/src/main/java/hi/verkefni/vinnsla/Ferd.java
@@ -1,4 +1,43 @@
 package hi.verkefni.vinnsla;
 
+import javafx.beans.property.*;
+import java.time.LocalDate;
+
+
 public class Ferd {
+
+//heiti ferðar ; áfangastað ; dagsetningu
+
+    private final StringProperty heitiFerdar = new SimpleStringProperty();
+    private final StringProperty afangastadur = new SimpleStringProperty();
+    private final ObjectProperty<LocalDate> dagsetning = new SimpleObjectProperty<>();
+
+    public Ferd(String heitiFerdar, String afangastadur, LocalDate dagsetning) {
+        this.heitiFerdar.set(heitiFerdar);
+        this.afangastadur.set(afangastadur);
+        this.dagsetning.set(dagsetning);
+    }
+
+    //generated getters frá intellij
+    public String getHeitiFerdar() {
+        return heitiFerdar.get();
+    }
+    public StringProperty heitiFerdarProperty() {
+        return heitiFerdar;
+    }
+
+    public String getAfangastadur() {
+        return afangastadur.get();
+    }
+    public StringProperty afangastadurProperty() {
+        return afangastadur;
+    }
+
+    public LocalDate getDagsetning() {
+        return dagsetning.get();
+    }
+    public ObjectProperty<LocalDate> dagsetningProperty() {
+        return dagsetning;
+    }
+
 }

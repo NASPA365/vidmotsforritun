d9ecc17cfc52fcca0356b5f470636ccea6058584
NASPA365 <ignass221@outlook.com>
2026-02-18 13:11:15 +0000

    klárað ferðaplan, gerði 3 random ferðaplön

--- Verkefni3/src/main/java/hi/verkefni/vinnsla/Ferdaplan.java
+++ Verkefni3/src/main/java/hi/verkefni/vinnsla/Ferdaplan.java
@@ -1,4 +1,18 @@
 package hi.verkefni.vinnsla;
 
+import javafx.collections.*;
+import java.time.LocalDate;
+
 public class Ferdaplan {
+
+    private final ObservableList<Ferd> ferdir = FXCollections.observableArrayList();
+
+    public Ferdaplan() {
+        ferdir.add(new Ferd("chill ferð","sveit", LocalDate.of(2026,3,15)));
+        ferdir.add(new Ferd("heimsækja ömmu og afa", "Litháen", LocalDate.of(2026,5,15)));
+        ferdir.add(new Ferd("afmælispartí á næsta ári", "Hawaii", LocalDate.of(2027,1,15)));
+    }
+
+    public ObservableList<Ferd> getFerdir() {return ferdir;}
+
 }
